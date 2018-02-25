8c5a2beda83ffdf6cab7385454c783fb4bf94d36
Crystal Fletcher <sporeluc@gmail.com>
2018-02-24 22:08:56 -0500

    What is the Bytestring?
    Figuring out how the bytestring works. Documentation makes sense, but the response that I am getting doesn't make sense.
    
    added websocket.sh to test, uses curl to test the java websocket

--- src/Main.java
+++ src/Main.java
@@ -51,7 +51,22 @@ public class Main {
                 "\r\n\r\n")
                 .getBytes("UTF-8");
 
+                System.out.println(response);
+
                 out.write(response, 0, response.length);
+
+                byte[] decoded = new byte[6];
+                byte[] encoded = new byte[] {(byte) 198, (byte) 131,
+                        (byte) 130, (byte) 182, (byte) 194, (byte) 135 };
+                byte[] key = new byte[] { (byte) 167, (byte) 225,
+                        (byte) 225, (byte) 210};
+
+                for ( int i = 0; i < encoded.length; i++ ) {
+                    decoded[i] = (byte)(encoded[i] ^ key[i & 0x3]);
+                }
+
+                System.out.println(decoded);
+
             } else {
 
             }
