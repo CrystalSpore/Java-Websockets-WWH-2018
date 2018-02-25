c0c320523e598cbd29ff7563e18fb09050ec114d
Crystal Fletcher <sporeluc@gmail.com>
2018-02-24 19:41:08 -0500

    Connection
    I (probably) have my connection stuff written and working, currently trying to figure out how to decode incoming "frames"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import javax.xml.bind.DatatypeConverter;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.MessageDigest;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -35,7 +37,21 @@ public class Main {
             Matcher get = Pattern.compile("^GET").matcher(data);
 
             if ( get.find() ) {
+                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+                match.find();
+                byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n" +
+                    "Connection: Upgrade\r\n" +
+                    "Upgrade: websocket\r\n" +
+                    "Sec-WebSocket-Accept: " +
+                    DatatypeConverter.printBase64Binary(
+                            MessageDigest
+                            .getInstance("SHA-1")
+                            .digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+                                .getBytes("UTF-8"))) +
+                "\r\n\r\n")
+                .getBytes("UTF-8");
 
+                out.write(response, 0, response.length);
             } else {
 
             }
